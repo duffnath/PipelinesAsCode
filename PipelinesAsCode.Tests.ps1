698ec38b221d17a3457663050b3da9fd442f6296
duffnath <duffnath@msu.edu>
2019-01-13 14:43:05 -0500

    Initial commit

---
+++ PipelinesAsCode.Tests.ps1
@@ -0,0 +1,26 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.ps1", ".psm1")
+
+Import-Module "$here\$sut"
+ 
+Describe "Get-AuthToken" {
+    $userName = "nate.duff@outlook.com"
+    $password = ConvertTo-SecureString $ENV:StandardPW -AsPlainText -Force
+
+    It "outputs a string with good credentials" {
+        $goodCreds = Get-Creds -userName $userName -password $password
+        $authToken = Get-AuthToken -creds $goodCreds
+
+        $authToken.GetType().Name | Should Be 'String'
+        $authToken.Length -gt 1 | Should -BeTrue
+    }
+
+    It "outputs an error with bad credentials" {
+        $badPassword = ConvertTo-SecureString "InvalidPassword" -AsPlainText -Force
+
+        $badCreds = Get-Creds -userName $userName -password $badPassword
+        $authToken = Get-AuthToken -creds $badCreds
+        
+        $authToken | Should -Not -Exist
+    }
+}
---
+++ Scripts/createPipeline.ps1
@@ -0,0 +1,62 @@
+# Script to create Build and Release defintions
+Param(
+    $userName = "nate.duff@outlook.com",
+    $organization = "NateDuff",
+    $project = "PipelinesAsCode",
+    $buildName = "PowerShell Build",
+    $releaseName = "PowerShell Release",
+    $manifestPath = "Build.yml",
+    $publicBuildVariables = @(
+        @{
+            Name = "BuildConfig"
+            Value = "Debug"
+        }
+    ),
+    $secretBuildVariables = @(
+        @{
+            Name = "StandardPW"
+            Value = $ENV:StandardPW
+        }
+    ),
+    $publicReleaseVariables = @(),
+    $secretReleaseVariables = @(
+        @{
+            Name = "StandardPW"
+            Value = $ENV:StandardPW
+        }
+    )
+)
+
+Import-Module ".\PipelinesAsCode.psm1"
+
+$secPassword = ConvertTo-SecureString $ENV:StandardPW -AsPlainText -Force
+
+$baseParams = @{
+    org = $organization 
+    project = $project    
+    creds = (Get-Creds $userName -password $secPassword)
+    buildName = $buildName
+}
+
+$buildParams = @{
+    manifestPath = $manifestPath
+    publicBuildVariables = $publicBuildVariables
+    secretBuildVariables = $secretBuildVariables
+}
+
+$build = New-BuildDefinition @baseParams @buildParams
+
+$releaseParams = @{
+    releaseName = $releaseName    
+    buildID = $build.id
+    projectID = $build.project.id
+    publicReleaseVariables = $publicReleaseVariables
+    secretReleaseVariables = $secretReleaseVariables
+}
+
+$release = New-ReleaseDefinition @baseParams @releaseParams
+
+$release
+
+#Remove-BuildDefinition -org $org -project $project -creds $creds -buildDefinitionID $build.id
+#Remove-ReleaseDefinition -org $org -project $project -creds $creds -releaseDefinitionID $release.id
