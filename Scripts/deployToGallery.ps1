e47fbe29fea2d2e7b0acfc1a701a87f48e0e97e5
duffnath <duffnath@msu.edu>
2019-01-30 19:13:57 -0500

    Oops

--- Scripts/deployToGallery.ps1
+++ Scripts/deployToGallery.ps1
@@ -4,6 +4,6 @@ Param(
     $modulePath = ".\CI Build\drop\Module"
 )
 
-Import-Module "$modulePath$module.psm1"
+Import-Module "$modulePath\$module.psm1"
 
 Publish-Module -Name $module -NuGetApiKey $galleryKey
